1889ee33e90015fadac3ae21202003ca7b08b737
Luciano Filippucci <filippucci.luciano@outlook.it>
2025-12-24 12:10:57 +0100

    Initial commit and config

---
+++ src/test/java/it/lucianofilippucci/tcgarkhive/TcgArkhiveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lucianofilippucci.tcgarkhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgArkhiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
